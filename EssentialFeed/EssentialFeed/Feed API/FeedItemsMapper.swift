82053907e559907fca142a02aaafe674d37564fa
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2022-12-05 09:23:02 +0530

    Refactor map logic

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -30,11 +30,11 @@ internal final class FeedItemsMapper {
     private static var OK_200: Int { 200 }
     
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+        guard response.statusCode == OK_200,
+                let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(.invalidData)
         }
 
-        let item = root.items.map{ $0.item }
-        return .success(item)
+        return .success(root.feed)
     }
 }
